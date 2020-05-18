a28ea644f8a98e0f2e785b65c4eac9276458c743
irvin-welsh <99cryptoman@gmail.com>
2020-05-18 08:20:32 +0300

    .

--- working_days_counter.py
+++ working_days_counter.py
@@ -4,6 +4,7 @@ from dateutil.rrule import *
 from dateutil.parser import *
 import calendar
 
+# Warehouse is working according to 1/3 shift (1 workday and 3 days off), so order must be delivered only on working days which are calculated as x+4
 def get_working_days():
     START_DATE= date(2020,5,20).strftime('%d-%B-%Y')
     delivery_dates=[]
@@ -11,4 +12,8 @@ def get_working_days():
     for slot in get_delivery_slots:
         slotFormatted = slot.strftime('%d-%B-%Y')
         delivery_dates.append(slotFormatted)
-    return delivery_dates
+    constant_line_in_message = "Свободные слоты на доставку:\n"
+    variable_lines_in_message = "\r\n\t".join(delivery_dates[1:])
+    concantenated_string = constant_line_in_message+variable_lines_in_message
+    return concantenated_string
+get_working_days()
