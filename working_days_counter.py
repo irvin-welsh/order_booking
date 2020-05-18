711dcd704a92d47bec6657de4ee58c14903420c7
irvin-welsh <99cryptoman@gmail.com>
2020-05-18 11:12:44 +0300

    .

--- working_days_counter.py
+++ working_days_counter.py
@@ -3,17 +3,37 @@ from dateutil.relativedelta import *
 from dateutil.rrule import *
 from dateutil.parser import *
 import calendar
+import sqlite3
+
+connection=sqlite3.connect("working_dates.db")
+crsr = connection.cursor()
+#
+# def getDataFromDB():
+#     for rows in crsr.execute('SELECT * FROM delivery_slots'):
+#         cc_rows=crsr.fetchall()
+#     working_dates=[cc_rows]
+#     return working_dates
 
 # Warehouse is working according to 1/3 shift (1 workday and 3 days off), so order must be delivered only on working days which are calculated as x+4
 def get_working_days():
-    START_DATE= date(2020,5,20).strftime('%d-%B-%Y')
+    current_date = date.today()
+    start_date = date(2020,5,20)
+    global delivery_dates
     delivery_dates=[]
-    get_delivery_slots=list(rrule(DAILY, interval=4, count=20, dtstart=parse(START_DATE)))
+    get_delivery_slots=list(rrule(DAILY, interval=4, count=100, dtstart=parse(str(start_date))))
     for slot in get_delivery_slots:
-        slotFormatted = slot.strftime('%d-%B-%Y')
+        slotFormatted = [slot.strftime('%d-%B-%Y')]
+        # tpl = tuple(slotFormatted)
         delivery_dates.append(slotFormatted)
-    constant_line_in_message = "Свободные слоты на доставку:\n"
-    variable_lines_in_message = "\r\n\t".join(delivery_dates[1:])
-    concantenated_string = constant_line_in_message+variable_lines_in_message
-    return concantenated_string
+    return delivery_dates
+
+def db_params():
+    params = [(k) for k in delivery_dates]
+    crsr.execute('''CREATE TABLE IF NOT EXISTS delivery_slots (Delivery_Slots TEXT);''')
+    for param in params:
+        z=tuple(param)
+        crsr.execute('INSERT INTO delivery_slots VALUES (?)', z)
+    connection.commit()
+
 get_working_days()
+db_params()
